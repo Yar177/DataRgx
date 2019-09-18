b50c7762a33afbac4507972b6b51439aa3583267
Yar177 <yarsher.dev@gmail.com>
2019-09-18 11:28:52 -0400

    maxLines

--- app/src/main/java/com/yar/datargx/Utils/DateTextInputUtils.java
+++ app/src/main/java/com/yar/datargx/Utils/DateTextInputUtils.java
@@ -3,8 +3,6 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 
-import com.google.android.material.textfield.TextInputEditText;
-
 import java.util.Calendar;
 
 public class DateTextInputUtils implements TextWatcher {
@@ -23,7 +21,6 @@ public class DateTextInputUtils implements TextWatcher {
         if (!s.toString().equals(current)) {
             String clean = s.toString().replaceAll("[^\\d.]|\\.", "");
             String cleanC = current.replaceAll("[^\\d.]|\\.", "");
-
             int cl = clean.length();
             int sel = cl;
             for (int i = 2; i <= cl && i < 6; i += 2) {
@@ -31,32 +28,22 @@ public class DateTextInputUtils implements TextWatcher {
             }
             //Fix for pressing delete next to a forward slash
             if (clean.equals(cleanC)) sel--;
-
-            if (clean.length() < 8){
+            if (clean.length() < 8) {
                 clean = clean + mmddyyyy.substring(clean.length());
-            }else{
-                //This part makes sure that when we finish entering numbers
-                //the date is correct, fixing it otherwise
-                int mon  = Integer.parseInt(clean.substring(0,2));
-                int day  = Integer.parseInt(clean.substring(2,4));
-                int year = Integer.parseInt(clean.substring(4,8));
-
+            } else {
+                int mon = Integer.parseInt(clean.substring(0, 2));
+                int day = Integer.parseInt(clean.substring(2, 4));
+                int year = Integer.parseInt(clean.substring(4, 8));
                 mon = mon < 1 ? 1 : mon > 12 ? 12 : mon;
-                cal.set(Calendar.MONTH, mon-1);
-                year = (year<1900)?1900:(year>2100)?2100:year;
+                cal.set(Calendar.MONTH, mon - 1);
+                year = (year < 1900) ? 1900 : (year > 2100) ? 2100 : year;
                 cal.set(Calendar.YEAR, year);
-                // ^ first set year for the line below to work correctly
-                //with leap years - otherwise, date e.g. 29/02/2012
-                //would be automatically corrected to 28/02/2012
-
-                day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
-                clean = String.format("%02d%02d%02d",mon, day, year);
+                day = (day > cal.getActualMaximum(Calendar.DATE)) ?
+                        cal.getActualMaximum(Calendar.DATE) : day;
+                clean = String.format("%02d%02d%02d", mon, day, year);
             }
-
-            clean = String.format("%s-%s-%s", clean.substring(0, 2),
-                    clean.substring(2, 4),
+            clean = String.format("%s-%s-%s", clean.substring(0, 2), clean.substring(2, 4),
                     clean.substring(4, 8));
-
             sel = sel < 0 ? 0 : sel;
             current = clean;
             if (current.equalsIgnoreCase("mm-dd-yyyy")) current = "";
@@ -67,14 +54,9 @@ public class DateTextInputUtils implements TextWatcher {
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-
     }
 
     @Override
     public void afterTextChanged(Editable s) {
     }
-
-
-
 }
